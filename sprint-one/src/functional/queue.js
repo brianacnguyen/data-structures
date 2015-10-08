fbc8e679d120d2de40ad621729f45a2274e70dda
Brian Nguyen <brianacnguyen@gmail.com>
2015-10-08 15:25:11 -0700

    completed queue-functional -Brian

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,6 +1,11 @@
 var Queue = function(){
   var someInstance = {};
-  var count = 0;
+  var sizeCount = 0;
+  var qCount = 0; 
+  var dqCount = 0;
+  //second count variable that starts at 0
+  // every time an item is dequeued second count gets incremented
+  //second count is used to get the index
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -8,21 +13,23 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
-    storage[count] = value; 
-    count ++;
+    storage[qCount] = value; 
+    sizeCount ++;
+    qCount ++; 
   };
 
   someInstance.dequeue = function(){
-    if(count > 0){
-      count --;
+    var result = storage[dqCount];
+    delete storage[dqCount];
+    if(sizeCount > 0){
+      sizeCount --;
     }
-    var result = storage[0];
-    delete storage[0];
+    dqCount ++; 
     return result; 
   };
 
   someInstance.size = function(){
-    return count;
+    return sizeCount;
   };
 
   return someInstance;
