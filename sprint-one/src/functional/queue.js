e05c4da6079ffaba2d15b0b4e37bb253641d8a7e
Brian Nguyen <brianacnguyen@gmail.com>
2015-10-08 10:51:29 -0700

    Attempted queue-functional -Brian

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,4 +1,5 @@
 var Queue = function(){
+  var count = 0; 
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -7,13 +8,21 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[count] = value;
+    count ++; 
   };
 
   someInstance.dequeue = function(){
+    if (count > 0) {
+      count --; 
+    }
+    var result = storage[0];
+    delete storage[0];
+    return result; 
   };
 
   someInstance.size = function(){
-    return 0; 
+    return count; 
   };
 
   return someInstance;
