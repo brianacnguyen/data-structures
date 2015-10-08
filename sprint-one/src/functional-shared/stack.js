8d30aabc40a36184062f3713e62d32b3a7ffa01b
Brian Nguyen <brianacnguyen@gmail.com>
2015-10-08 16:39:43 -0700

    Completed queue and stack -Brian

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // need push, pop, size 
+  var someInstance = {count: 0};
+  _.extend(someInstance, stackMethods);
+  return someInstance; 
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+  	this[this.count] = value;
+  	this.count++;
+  },
+
+  pop: function(){
+  	if (this.count > 0) {
+  		this.count--; 
+  	}
+  	var result = this[this.count];
+  	delete this[this.count];
+  	return result;
+  },
+
+  size: function(){
+  	return this.count;  
+  }
+
+};
 
 
