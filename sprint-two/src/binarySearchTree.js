326dcde79ab298a7e94bd3eaa5ba51ddd787c6e3
Brian Nguyen <brianacnguyen@gmail.com>
2015-10-10 16:26:38 -0700

    Added comments and cleaned up code -Brian

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,68 +1,82 @@
-//trees that can only have 0,1,or 2 children
-//recursive
-//1 child will always have value less than parent value and 1 will always have greater than
-//great for dictionary of all english words
-//need a left property, a binary search tree where all values are lower than current value
-//need right = higher
-//need insert() accepts value and places in the tree in the correction position
-//need contains()
-//need depthFirstLog() accepts callback and executes it on every value contained in the tree
-//Given a list of a million numbers, write a function that takes a new number and returns the closest number in the list using your BST 
-//Profile this against the same operation using an array.
 var BinarySearchTree = function(value){
+	//creating tree object that stores value, left, and right tree
 	var tree = {value: value, left: null, right: null};
+	//adding searchTreeMethods to tree object
 	_.extend(tree, searchTreeMethods);
+	//return tree 
 	return tree;
 };
 
 var searchTreeMethods = {}; 
+//adding insert method to searchTreeMethods
 searchTreeMethods.insert = function(val,branch){
+	// if branch is not defined, use current branch (main branch)
 	branch = (branch === undefined) ? this : branch;
+	// if target value (val) is less than the value of the current branch, check left 
 	if (val < branch.value) {
+		// if there is no left tree, create left tree
 		if (branch.left === null) {
 			branch.left = BinarySearchTree(val,branch.left);
 		}
+		// if there is already a left tree, compare again and insert accordingly
 		else {
 			branch.insert(val,branch.left);
 		}
 	}
+	// if target value (val) is greater than the value of the current branch, check right
 	else if (val > branch.value) {
+		// if there is no right tree, create right tree
 		if (branch.right === null) { 
 			branch.right = BinarySearchTree(val,branch.right);
 		}
+		// if there is already a right tree, compare again and insert accordingly
 		else {
 			branch.insert(val,branch.right);	
 		}
 	}
 };
+//adding contains method to searchTreeMethods
 searchTreeMethods.contains = function(target, branch){
+	// if branch is not defined, use current branch (main branch)
 	branch = (branch === undefined) ? this : branch;
+	// declaring result var to store result and return later
 	var result = []
+	// test if current branch has value and push to result
 	var test = (branch.value === target)
 	result.push (test); 
+	// if target is less than branch value and there is a left branch, check left branch's children and push to result
 	if (target < branch.value && branch.left !== null) {
 		var leftResult = branch.contains(target, branch.left);
 		result.push(leftResult);	
 	}
+	// if target is greater than branch value and there is a right branch, check right branch's children and push to result
 	else if (target > branch.value && branch.right !== null) {
 		var rightResult = branch.contains(target, branch.right);
 		result.push(rightResult);
 	}
+	//check if there is a true in the result and return
 	result = _.some(result); 
 	return result;
 };
+//adding depthFirstLog method to searchTreeMethods
 searchTreeMethods.depthFirstLog = function(func,branch){
+	// if branch is not defined, use current branch (main branch)
 	branch = (branch === undefined) ? this : branch;
+	// declaring result var to store result and return later
 	var result = []; 
+	// use callback on value of first branch 
 	result.push(func(branch.value));
+	//if there is a left branch, use callback or left branch and its children 
 	if (branch.left !== null) {
 		var leftResult = branch.depthFirstLog(func, branch.left);
 		result.push(leftResult);
 	}
+	//if there is a right branch, use callback or right branch and its children 
 	else if (branch.right !== null) {
 		var rightResult = branch.depthFirstLog(func, branch.right);
 		result.push(rightResult);
 	}
+	//return result array with callbacks of all values
 	return result; 
 
 };
@@ -71,4 +85,5 @@ searchTreeMethods.depthFirstLog = function(func,branch){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ 	logarithmic
  */
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,31 +1,26 @@
-//Graph Solution
-// Instantiate a new graph
 var Graph = function(){
 	this.nodeStorage = {};
 };
-/*
-- consisted of nodes (or vertices) and edges(arcs) that connect the nodes
-- not hierarchical
-- can be undirected (relationship between 2 nodes = symmetrical) or directed
-- we will be implemented undirected
-- pseudoclassical style
-- similar to how a web works
-*/
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node){
+	//sets key node to a new empty array
 	this.nodeStorage[node] = [];
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node){
+	//result begins a with a value until the node is found
 	var result = false; 
+	//iterate through the graph's nodeStorage
 	for (var key in this.nodeStorage) {
+		//if there is a key that matches node, return true
 		if (key === node) {
 			result = true;
 		}
 	}
+	//returns the value of result, which gets re-assigned to true when a match is found otherwise returns false
 	return result; 
 };
 
@@ -34,7 +29,9 @@ Graph.prototype.contains = function(node){
 Graph.prototype.removeNode = function(node){
 	//takes node and removes from graph(if present)
 	//all edges connected to node are removed too
+	//iterate over nodeStorage to see if there is a key that matches node
 	for (var key in this.nodeStorage){
+		//check to see if a key matches node, if true delete the value stored at node
 		if (key === node){
 			delete this.nodeStorage[node];
 		}
@@ -44,11 +41,15 @@ Graph.prototype.removeNode = function(node){
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode){
+	//iterate through nodeStorage
 	for (var key in this.nodeStorage){
+		//check to see if any keys match fromNode
 		if (key === fromNode){
+			//if a match is found, check to see if the stored value(s) at fromNode equal to toNode and return true
 			if (this.nodeStorage[fromNode].indexOf(toNode) >= 0) {
 				return true; 
 			}
+			//if there are no matches found return false
 			else {
 				return false;
 			} 
@@ -59,11 +60,16 @@ Graph.prototype.hasEdge = function(fromNode, toNode){
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode){ //fromNode = penguins, toNode = puppies
+	//iterate through nodeStorage
 	for (var key in this.nodeStorage){
+		//check to see if there are any keys that match fromNode
 		if (key === fromNode){
+			//if a match is found, push toNode into the empty array stored in fromNode
 			this.nodeStorage[fromNode].push(toNode); //{penguins: ['puppies']}
 		}
+		//if a key does not match fromNode, check to see if any keys match toNode
 		else if (key === toNode){
+			//if a match is found, push fromNode into the empty array stored in toNode
 			this.nodeStorage[toNode].push(fromNode); //{puppies: ['penguins']}
 		}
 	}
@@ -72,29 +78,36 @@ Graph.prototype.addEdge = function(fromNode, toNode){ //fromNode = penguins, toN
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode){
-	for (var key in this.nodeStorage){
+   //iterate over nodeStorage
+   	for (var key in this.nodeStorage){
+   		//check to see if there are any keys that match fromNode
 		if (key === fromNode){
+			//get the index of toNode in fromNode and remove it using splice
 			var index = this.nodeStorage[fromNode].indexOf(toNode); 
 			this.nodeStorage[fromNode].splice(index,1); 
 		}
+		//if a key does not match fromNode, check to see if any keys match toNode
 		else if (key === toNode){
+			//get the index of fromNode in toNode and remove it using splice
 			var index = this.nodeStorage[toNode].indexOf(fromNode); 
 			this.nodeStorage[toNode].splice(index, 1);
 		}
 	}
-
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb){
+	//iterates over nodeStorage
 	for (var key in this.nodeStorage) {
+		//invokes the callback on the key
 		cb(key); 
 	}
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ 	linear
  */
 
 
